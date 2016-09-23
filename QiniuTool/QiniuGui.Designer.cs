dabbc6e9dcdf68a67b5cfeb64ada85d2162ba8bd
NlsNi <1245014061@qq.com>
2016-09-23 23:57:43 +0800

    first commit with both README.md and codes

---
+++ QiniuTool/QiniuGui.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace QiniuTool
+{
+    partial class QiniuGui
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.buttonUpload = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonPic = new System.Windows.Forms.Button();
+            this.textBoxPic = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.buttonUrl = new System.Windows.Forms.Button();
+            this.textBoxUrl = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonWatch = new System.Windows.Forms.Button();
+            this.textBoxWatch = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.statusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.buttonUpload);
+            this.groupBox1.Controls.Add(this.buttonWatch);
+            this.groupBox1.Controls.Add(this.textBoxWatch);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.buttonPic);
+            this.groupBox1.Controls.Add(this.textBoxPic);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(344, 359);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "单文件上传";
+            // 
+            // buttonUpload
+            // 
+            this.buttonUpload.Location = new System.Drawing.Point(287, 28);
+            this.buttonUpload.Name = "buttonUpload";
+            this.buttonUpload.Size = new System.Drawing.Size(51, 96);
+            this.buttonUpload.TabIndex = 6;
+            this.buttonUpload.Text = "上传";
+            this.buttonUpload.UseVisualStyleBackColor = true;
+            this.buttonUpload.Click += new System.EventHandler(this.buttonUpload_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(8, 153);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(330, 200);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.DoubleClick += new System.EventHandler(this.buttonPic_Click);
+            // 
+            // buttonPic
+            // 
+            this.buttonPic.Location = new System.Drawing.Point(232, 28);
+            this.buttonPic.Name = "buttonPic";
+            this.buttonPic.Size = new System.Drawing.Size(49, 23);
+            this.buttonPic.TabIndex = 2;
+            this.buttonPic.Text = "浏览";
+            this.buttonPic.UseVisualStyleBackColor = true;
+            this.buttonPic.Click += new System.EventHandler(this.buttonPic_Click);
+            // 
+            // textBoxPic
+            // 
+            this.textBoxPic.Location = new System.Drawing.Point(65, 29);
+            this.textBoxPic.Name = "textBoxPic";
+            this.textBoxPic.Size = new System.Drawing.Size(159, 21);
+            this.textBoxPic.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "图片路径";
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip.Location = new System.Drawing.Point(0, 374);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(368, 22);
+            this.statusStrip.TabIndex = 2;
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(20, 17);
+            this.toolStripStatusLabel1.Text = "   ";
+            // 
+            // buttonUrl
+            // 
+            this.buttonUrl.Location = new System.Drawing.Point(244, 114);
+            this.buttonUrl.Name = "buttonUrl";
+            this.buttonUrl.Size = new System.Drawing.Size(49, 23);
+            this.buttonUrl.TabIndex = 6;
+            this.buttonUrl.Text = "复制";
+            this.buttonUrl.UseVisualStyleBackColor = true;
+            this.buttonUrl.Click += new System.EventHandler(this.buttonUrl_Click);
+            // 
+            // textBoxUrl
+            // 
+            this.textBoxUrl.Location = new System.Drawing.Point(77, 115);
+            this.textBoxUrl.Name = "textBoxUrl";
+            this.textBoxUrl.Size = new System.Drawing.Size(159, 21);
+            this.textBoxUrl.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "外链地址";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "监测路径";
+            // 
+            // buttonWatch
+            // 
+            this.buttonWatch.Location = new System.Drawing.Point(232, 64);
+            this.buttonWatch.Name = "buttonWatch";
+            this.buttonWatch.Size = new System.Drawing.Size(49, 23);
+            this.buttonWatch.TabIndex = 5;
+            this.buttonWatch.Text = "浏览";
+            this.buttonWatch.UseVisualStyleBackColor = true;
+            this.buttonWatch.Click += new System.EventHandler(this.buttonWatch_Click);
+            // 
+            // textBoxWatch
+            // 
+            this.textBoxWatch.Location = new System.Drawing.Point(65, 65);
+            this.textBoxWatch.Name = "textBoxWatch";
+            this.textBoxWatch.Size = new System.Drawing.Size(159, 21);
+            this.textBoxWatch.TabIndex = 4;
+            // 
+            // QiniuGui
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(368, 396);
+            this.Controls.Add(this.buttonUrl);
+            this.Controls.Add(this.textBoxUrl);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "QiniuGui";
+            this.Text = "七牛上传工具";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.TextBox textBoxPic;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonPic;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonUrl;
+        private System.Windows.Forms.TextBox textBoxUrl;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Button buttonUpload;
+        private System.Windows.Forms.Button buttonWatch;
+        private System.Windows.Forms.TextBox textBoxWatch;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ QiniuTool/QiniuGui.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Qiniu.Auth;
+using Qiniu.IO;
+using Qiniu.IO.Resumable;
+using Qiniu.RS;
+
+
+namespace QiniuTool
+{
+    public partial class QiniuGui : Form
+    {
+        public QiniuGui()
+        {
+            InitializeComponent();        
+        }
+
+        private void buttonPic_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            string picPath = null;
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                picPath = fileDialog.FileName.ToString();
+                textBoxPic.Text = picPath;
+            }
+            if (picPath != null)
+            {
+                picPath = picPath.ToLower();
+                if (picPath.EndsWith("jpg") | picPath.EndsWith("png") |
+                    picPath.EndsWith("bmp") | picPath.EndsWith("gif"))
+                {
+                    pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+                    pictureBox1.Image = Image.FromFile(picPath);
+                }
+            }
+
+        }
+
+        private void buttonWatch_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folder = new FolderBrowserDialog();
+            string watchPath = null;
+            if (folder.ShowDialog() == DialogResult.OK)
+            {
+                watchPath = folder.SelectedPath.ToString();
+                textBoxWatch.Text = watchPath;
+                if (watchPath != null)
+                {
+                    FileSystemWatcher watcher = new FileSystemWatcher();
+                    watcher.Filter = "*.*";
+                    watcher.Path = watchPath;
+                    watcher.EnableRaisingEvents = true;
+                    watcher.IncludeSubdirectories = true;
+                    watcher.Created += OnChanged;
+                    watcher.Changed += OnChanged;
+                    watcher.Renamed += OnChanged;
+                }         
+            }
+               
+        }
+
+        private  void OnChanged(object source, FileSystemEventArgs e)
+        {
+            string filepath = e.FullPath;
+            string filename = e.Name;
+            upload(filename, filepath);
+            
+        }
+
+        private void upload(string filename,string filepath)
+        {
+            //设置AK SK
+            Qiniu.Conf.Config.ACCESS_KEY = "ACCESS_KEY";
+            Qiniu.Conf.Config.SECRET_KEY = "SECRET_KEY";
+            IOClient target = new IOClient();
+            PutExtra extra  = new PutExtra();
+            string bucket = "bucketname";
+            string key = filename;
+            PutPolicy put = new PutPolicy(bucket, 3600);
+            string upToken = put.Token();
+            PutRet ret = target.PutFile(upToken,key,filepath,extra);
+            toolStripStatusLabel1.Text = ret.Response.ToString();
+            string url= @"http://example.bkt.clouddn.com/" + filename;
+            textBoxUrl.Text = url;
+        }
+
+        private void buttonUrl_Click(object sender, EventArgs e)
+        {
+            Clipboard.Clear();
+            string url = textBoxUrl.Text;
+            if (url.Length != 0)
+            {
+                Clipboard.SetText(url);
+                toolStripStatusLabel1.Text = "外链地址已复制到剪贴板";
+            }
+
+        }
+
+        private void buttonUpload_Click(object sender, EventArgs e)
+        {
+            string filepath = textBoxPic.Text;
+            string ending = filepath.Split('.')[1];
+            string filename = string.Format("{0:yyyyMMdd_HHmmss}",DateTime.Now) + "." + ending;
+            upload(filename, filepath);
+        }
+    }
+}
