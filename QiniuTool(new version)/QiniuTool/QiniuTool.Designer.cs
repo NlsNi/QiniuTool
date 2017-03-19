05d83165ec1a0db50d83baaac550730a10c69ea0
NlsNi <1245014061@qq.com>
2017-03-19 21:25:26 +0800

    added new version

---
+++ QiniuTool(new version)/QiniuTool/QiniuTool.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace QiniuTool
+{
+    partial class QiniuGui
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.buttonUpload = new System.Windows.Forms.Button();
+            this.buttonCopy = new System.Windows.Forms.Button();
+            this.buttonExplore = new System.Windows.Forms.Button();
+            this.textBoxUrl = new System.Windows.Forms.TextBox();
+            this.textBoxPath = new System.Windows.Forms.TextBox();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBoxBuckets = new System.Windows.Forms.ComboBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.statusStrip.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel});
+            this.statusStrip.Location = new System.Drawing.Point(0, 372);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(640, 22);
+            this.statusStrip.TabIndex = 2;
+            this.statusStrip.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel
+            // 
+            this.toolStripStatusLabel.Name = "toolStripStatusLabel";
+            this.toolStripStatusLabel.Size = new System.Drawing.Size(20, 17);
+            this.toolStripStatusLabel.Text = "   ";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.buttonUpload);
+            this.groupBox1.Controls.Add(this.buttonCopy);
+            this.groupBox1.Controls.Add(this.buttonExplore);
+            this.groupBox1.Controls.Add(this.textBoxUrl);
+            this.groupBox1.Controls.Add(this.textBoxPath);
+            this.groupBox1.Controls.Add(this.pictureBox);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.comboBoxBuckets);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(313, 357);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "文件上传";
+            // 
+            // buttonUpload
+            // 
+            this.buttonUpload.Location = new System.Drawing.Point(230, 96);
+            this.buttonUpload.Name = "buttonUpload";
+            this.buttonUpload.Size = new System.Drawing.Size(60, 23);
+            this.buttonUpload.TabIndex = 7;
+            this.buttonUpload.Text = "上传";
+            this.buttonUpload.UseVisualStyleBackColor = true;
+            this.buttonUpload.Click += new System.EventHandler(this.buttonUpload_Click);
+            // 
+            // buttonCopy
+            // 
+            this.buttonCopy.Location = new System.Drawing.Point(230, 66);
+            this.buttonCopy.Name = "buttonCopy";
+            this.buttonCopy.Size = new System.Drawing.Size(60, 23);
+            this.buttonCopy.TabIndex = 6;
+            this.buttonCopy.Text = "复制";
+            this.buttonCopy.UseVisualStyleBackColor = true;
+            this.buttonCopy.Click += new System.EventHandler(this.buttonCopy_Click);
+            // 
+            // buttonExplore
+            // 
+            this.buttonExplore.Location = new System.Drawing.Point(230, 32);
+            this.buttonExplore.Name = "buttonExplore";
+            this.buttonExplore.Size = new System.Drawing.Size(60, 23);
+            this.buttonExplore.TabIndex = 5;
+            this.buttonExplore.Text = "浏览";
+            this.buttonExplore.UseVisualStyleBackColor = true;
+            this.buttonExplore.Click += new System.EventHandler(this.buttonExplore_Click);
+            // 
+            // textBoxUrl
+            // 
+            this.textBoxUrl.Location = new System.Drawing.Point(80, 66);
+            this.textBoxUrl.Name = "textBoxUrl";
+            this.textBoxUrl.Size = new System.Drawing.Size(144, 21);
+            this.textBoxUrl.TabIndex = 0;
+            // 
+            // textBoxPath
+            // 
+            this.textBoxPath.Location = new System.Drawing.Point(80, 34);
+            this.textBoxPath.Name = "textBoxPath";
+            this.textBoxPath.Size = new System.Drawing.Size(144, 21);
+            this.textBoxPath.TabIndex = 0;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(6, 137);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(301, 214);
+            this.pictureBox.TabIndex = 4;
+            this.pictureBox.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 10F);
+            this.label3.Location = new System.Drawing.Point(11, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 14);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Buckets";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 10F);
+            this.label2.Location = new System.Drawing.Point(11, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 14);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "外链地址";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 10F);
+            this.label1.Location = new System.Drawing.Point(11, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 14);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "图片路径";
+            // 
+            // comboBoxBuckets
+            // 
+            this.comboBoxBuckets.FormattingEnabled = true;
+            this.comboBoxBuckets.Location = new System.Drawing.Point(80, 98);
+            this.comboBoxBuckets.Name = "comboBoxBuckets";
+            this.comboBoxBuckets.Size = new System.Drawing.Size(144, 20);
+            this.comboBoxBuckets.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dataGridView);
+            this.groupBox2.Location = new System.Drawing.Point(340, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(288, 357);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "图片列表";
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(7, 14);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowTemplate.Height = 23;
+            this.dataGridView.Size = new System.Drawing.Size(275, 337);
+            this.dataGridView.TabIndex = 0;
+            this.dataGridView.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView_CellMouseClick);
+            this.dataGridView.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.dataGridView_MouseDoubleClick);
+            // 
+            // QiniuGui
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(640, 394);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.statusStrip);
+            this.MaximizeBox = false;
+            this.Name = "QiniuGui";
+            this.Text = "七牛上传工具";
+            this.Load += new System.EventHandler(this.QiniuGui_Load);
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox comboBoxBuckets;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.TextBox textBoxUrl;
+        private System.Windows.Forms.TextBox textBoxPath;
+        private System.Windows.Forms.Button buttonUpload;
+        private System.Windows.Forms.Button buttonCopy;
+        private System.Windows.Forms.Button buttonExplore;
+    }
+}
+
---
+++ QiniuTool(new version)/QiniuTool/QiniuTool.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Configuration;
+using System.IO;
+using System.Security.Cryptography;
+
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+using Qiniu.Common;
+using Qiniu.RS;
+using Qiniu.RS.Model;
+using Qiniu.Http;
+using Qiniu.Util;
+using Qiniu.IO;
+using Qiniu.IO.Model;
+
+namespace QiniuTool
+{
+    public partial class QiniuGui : Form
+    {
+        private static Mac mac = null;
+        public QiniuGui()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void QiniuGui_Load(object sender, EventArgs e)
+        {
+            //获取所有Buckets
+            var appSettings = ConfigurationManager.AppSettings;
+            string AK = appSettings["AK"];
+            string SK = appSettings["SK"];
+            mac = new Mac(AK,SK);
+            BucketManager bucketManger = new BucketManager(mac);
+            BucketsResult bucketResult = bucketManger.Buckets();
+            for (int i = 0; i < bucketResult.Result.Count; i++)
+            {
+                comboBoxBuckets.Items.Add(bucketResult.Result[i]);
+            }
+        }
+
+        private void buttonExplore_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            string picPath = null;
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                picPath = fileDialog.FileName.ToString();
+                this.textBoxPath.Text = picPath;
+            }
+            if (!String.IsNullOrEmpty(picPath))
+            {
+                picPath =picPath.ToLower();
+                if (picPath.EndsWith("jpg") || picPath.EndsWith("png") ||
+                    picPath.EndsWith("bmp") || picPath.EndsWith("gif"))
+                {
+                    this.pictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+                    this.pictureBox.Image = Image.FromFile(picPath);
+                }
+            }
+        }
+
+        private void buttonCopy_Click(object sender, EventArgs e)
+        {
+            Clipboard.Clear();
+            string url = this.textBoxUrl.Text;
+            if(!String.IsNullOrEmpty(url))
+            {
+                Clipboard.SetText(url);
+                this.toolStripStatusLabel.Text = "外链地址已复制到剪贴板";
+            }
+
+        }
+
+        private void buttonUpload_Click(object sender, EventArgs e)
+        {
+
+            string filepath = this.textBoxPath.Text;
+            if (String.IsNullOrEmpty(filepath))
+            {
+                MessageBox.Show("未选择要上传的文件");
+                this.textBoxPath.Focus();
+                return;
+            }
+            else
+            {
+                //如果hash值在数据库中存在，证明已经上传过该文件
+                string hash = GetMD5Hash(filepath);
+                using (MySqlConnection connection = new MySqlConnection())
+                {
+                    string connectionString = ConfigurationManager.ConnectionStrings["qiniu"].ConnectionString;
+                    connection.ConnectionString = connectionString;
+                    connection.Open();
+                    string sqlQuery = "SELECT COUNT(id) FROM pictures WHERE HashValue = " + "'" + hash + "'";
+                    MySqlCommand cmd = new MySqlCommand(sqlQuery, connection);
+                    Object result = cmd.ExecuteScalar();
+                    if (Convert.ToInt32(result) != 0)
+                    {
+                        this.toolStripStatusLabel.Text = "已上传过该文件";
+                    }
+                    else
+                    {
+                        string ending = filepath.Split('.')[1];
+                        string filename = String.Format("{0:yyyymmdd_hhmmss}", DateTime.Now) + "." + ending;
+                        this.Upload(filename, filepath);
+                        this.ShowAllPictures();
+                    }
+                }  
+            }
+        }
+
+        private string GetMD5Hash(string filepath)
+        {
+            StringBuilder builder = new StringBuilder();
+            using (FileStream fs = new FileStream(filepath, FileMode.Open, FileAccess.Read))
+            {
+                MD5 md5 = MD5.Create();
+                byte[] data = md5.ComputeHash(fs);
+                for (int i = 0; i < data.Length;i++ )
+                {
+                    builder.Append(data[i].ToString("x2"));
+                }
+            }   
+            return builder.ToString();
+        }
+        
+        private void Upload(string filename,string filepath)
+        {
+            string bucket;
+            if (this.comboBoxBuckets.SelectedIndex ==-1)
+            {
+                bucket = ConfigurationManager.AppSettings["defaultbucket"];
+            }
+            else
+	        {
+                bucket = this.comboBoxBuckets.SelectedItem.ToString();
+	        }
+            PutPolicy putPolicy = new PutPolicy();
+            putPolicy.Scope = bucket;
+            putPolicy.SetExpires(3600);
+            string jsonStr = putPolicy.ToJsonString();
+            string token = Auth.CreateUploadToken(mac, jsonStr);
+            BucketManager bucketManager = new BucketManager(mac);
+            string domain = bucketManager.Domains(bucket).Result[0];
+            FormUploader formUploader = new FormUploader();
+            HttpResult result = formUploader.UploadFile(filepath,filename,token);
+            if(result.RefCode ==200)
+            {
+                this.toolStripStatusLabel.Text = "上传成功";
+                #region UpdateDatabase
+
+                string uploadTime = String.Format("{0:yyyy-mm-dd hh:mm:ss}", DateTime.Now);
+                string hashValue = GetMD5Hash(filepath);
+                string downloadUrl = "http://" + domain + "/"+ filename;
+                this.textBoxUrl.Text = downloadUrl;
+                string localPath = filepath;
+                
+                string connectionString = ConfigurationManager.ConnectionStrings["qiniu"].ConnectionString;
+                try
+                {
+                    using (MySqlConnection connection = new MySqlConnection(connectionString))
+                    {
+                        connection.Open();
+                        string sqlInsert = "INSERT INTO pictures(Bucket,HashValue,UploadTime,DownloadUrl,LocalPath,Description)" +
+                                    "values(@Bucket,@HashValue,@UploadTime,@DownloadUrl,@LocalPath,@Description)";
+                        MySqlCommand cmd = new MySqlCommand(sqlInsert, connection);
+                        cmd.Parameters.AddWithValue("@Bucket", bucket);
+                        cmd.Parameters.AddWithValue("@HashValue", hashValue);
+                        cmd.Parameters.AddWithValue("@UploadTime", uploadTime);
+                        cmd.Parameters.AddWithValue("@DownloadUrl", downloadUrl);
+                        cmd.Parameters.AddWithValue("@LocalPath", localPath);
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    this.toolStripStatusLabel.Text = ex.Message + "数据库操作失败，请检查设置";
+                }
+                #endregion
+            }
+        }
+
+        private void dataGridView_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.ShowAllPictures();
+        }
+        private void ShowAllPictures()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["qiniu"].ConnectionString;
+            using(MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+                string sqlQuery = "SELECT * FROM pictures";
+                MySqlDataAdapter adapter = new MySqlDataAdapter(sqlQuery,connection);
+                MySqlCommandBuilder commandBuilder = new MySqlCommandBuilder(adapter);
+                DataSet dataSet = new DataSet();
+                adapter.Fill(dataSet,"pictures");
+                this.dataGridView.DataSource = dataSet;
+                this.dataGridView.DataMember = "pictures";
+            }
+        }
+
+        private void dataGridView_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            string localPath = this.dataGridView.CurrentRow.Cells[5].Value.ToString();
+            if (File.Exists(localPath))
+            {
+                this.pictureBox.Image = Image.FromFile(localPath);
+                this.textBoxUrl.Text = this.dataGridView.CurrentRow.Cells[4].Value.ToString();
+            }
+        }
+    }
+}
